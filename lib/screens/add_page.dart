18ec6e2dbe6d8c742445bf89f4d94fece73b34a5
Gokarna Khanal <khanalg358@gmail.com>
2023-07-04 00:09:32 +0545

    crud opertaion is done

--- lib/screens/add_page.dart
+++ lib/screens/add_page.dart
@@ -1,56 +1,90 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:http/http.dart'as http;
+import 'package:http/http.dart' as http;
 
 class AddToPage extends StatefulWidget {
-  const AddToPage({super.key});
+  const AddToPage({Key? key, required Map<String, dynamic> item})
+      : super(key: key);
 
   @override
-  State<AddToPage> createState() => _AddToPageState();
+  _AddToPageState createState() => _AddToPageState();
 }
 
 class _AddToPageState extends State<AddToPage> {
-  TextEditingController titleControler=TextEditingController();
-  TextEditingController descriptionControler=TextEditingController();
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Add todo list"),
       ),
-      body: ListView(padding: const EdgeInsets.all(20), children:  [
-         TextField(
-          controller: titleControler,
-          decoration: InputDecoration(hintText: 'title'),
-        ),
-        SizedBox(height: 30,),
-         TextField(
-          controller: descriptionControler,
-          decoration: InputDecoration(hintText: 'description'),
-          minLines: 5,
-          maxLines: 8,
-        ),
-        SizedBox(height: 30,),
-        ElevatedButton(onPressed: (){}, child: const Text("add"))
-      ]),
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        children: [
+          TextField(
+            controller: titleController,
+            decoration: InputDecoration(hintText: 'title'),
+          ),
+          SizedBox(height: 30),
+          TextField(
+            controller: descriptionController,
+            decoration: InputDecoration(hintText: 'description'),
+            minLines: 5,
+            maxLines: 8,
+          ),
+          SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: submitData,
+            child: const Text("add"),
+          ),
+        ],
+      ),
     );
   }
-  void submitData()async{
-    final title=titleControler.text;
-    final description=descriptionControler.text;
-    final body={
-  "title": "title",
-  "description": "description",
-  "is_completed": false
-};
-final url="https://api.nstack.in/v1/todos";
-final uri=Uri.parse(url);
-final response=await http.post(uri,body: jsonEncode(body));
-print(response);
 
+  void submitData() async {
+    final title = titleController.text;
+    final description = descriptionController.text;
+    final body = {
+      "title": title,
+      "description": description,
+      "is_completed": false,
+    };
 
+    // Submit data to the server
+    final url = "https://api.nstack.in/v1/todos";
+    final uri = Uri.parse(url);
+    final response = await http.post(
+      uri,
+      body: jsonEncode(body),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 201) {
+      titleController.text = '';
+      descriptionController.text = '';
+      showSuccessMessage("Create Success");
+    } else {
+      showErrorMessage("Creation failed");
+      print(response);
+    }
+  }
+
+  void showSuccessMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(message),
+      backgroundColor: Colors.pink,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void showErrorMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(message),
+      backgroundColor: Colors.red,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -1,36 +1,134 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:http/http.dart' as http;
 import 'package:todo_app/screens/add_page.dart';
 
 class TodoList extends StatefulWidget {
-  const TodoList({super.key});
+  const TodoList({Key? key}) : super(key: key);
 
   @override
   State<TodoList> createState() => _TodoListState();
 }
 
 class _TodoListState extends State<TodoList> {
+  List<dynamic> items = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchTodo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
         title: const Text(
-          ("Todo App"),
+          "Todo App",
           style: TextStyle(
-              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: navigateToAddPage,
-      child: const Icon(Icons.add),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: navigateToAddPage,
+        child: const Icon(Icons.add),
+      ),
+      body: items.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                final item = items[index];
+                final id = item['_id'] as String;
+
+                return Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      child: Text('${index + 1}'),
+                    ),
+                    title: Text(item['title']),
+                    subtitle: Text(item['description']),
+                    trailing: PopupMenuButton(
+                      onSelected: (value) {
+                        if (value == 'edit') {
+                          // Open the edit page
+                          navigateToUpdate(item);
+                        } else if (value == 'delete') {
+                          // Delete and refresh the page
+                          delete(id);
+                        }
+                      },
+                      itemBuilder: (context) {
+                        return [
+                          PopupMenuItem(
+                            child: Text('Edit'),
+                            value: 'edit',
+                          ),
+                          PopupMenuItem(
+                            child: Text('Delete'),
+                            value: 'delete',
+                          )
+                        ];
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
     );
   }
-  void navigateToAddPage(){
-    final route=MaterialPageRoute(builder: (context)=>AddToPage(),
-    
-    );
-    Navigator.push(context, route);
 
+  void navigateToAddPage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => AddToPage(
+                item: {},
+              )),
+    ).then((_) {
+      fetchTodo();
+    });
+  }
+
+  Future<void> navigateToUpdate(Map<String, dynamic> item) async {
+    await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => AddToPage(item: item)),
+    ).then((_) {
+      fetchTodo();
+    });
+  }
+
+  Future<void> delete(String id) async {
+    final url = "https://api.nstack.in/v1/todos/$id";
+    final uri = Uri.parse(url);
+
+    final response = await http.delete(uri);
+    if (response.statusCode == 200) {
+      setState(() {
+        items = items.where((element) => element['_id'] != id).toList();
+      });
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+  Future<void> fetchTodo() async {
+    final url = "https://api.nstack.in/v1/todos?page=1&limit=10";
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as Map<String, dynamic>;
+      final result = json['items'] as List<dynamic>;
+      setState(() {
+        items = result;
+      });
+    } else {
+      print(response.statusCode);
+    }
   }
 }
